c2db737825ec2bbbd38bfff9d504b0e781838b5c
CezGain <benjamin.vrnt@gmail.com>
2024-11-18 15:57:57 +0100

    fix: cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import { Hono } from "hono";
 import { routes } from "./routes";
+import { cors } from "hono/cors";
 
 const app = new Hono().basePath("/api");
+
+app.use("*", cors());
 
 app.route("", routes);
 
